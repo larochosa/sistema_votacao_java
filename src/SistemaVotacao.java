bdd7b8ca569c5c8f43c6ebbaf9d165debddb5e6f
Laura Rocha <laura@email.com>
2026-08-21 13:14:37 -0300

    chore: iniciar estrutura do projeto de votação

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class SistemaVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
 }
