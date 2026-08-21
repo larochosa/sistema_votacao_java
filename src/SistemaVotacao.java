e91d593902c3db1e869c9eff52c0c0c02a2bde07
larochosa <laura.11780074@aluno.mg.gov.br>
2026-08-21 14:09:12 -0300

    feat: adicionar leitura segura de números inteiros

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -18,6 +18,20 @@ public class SistemaVotacao {
     static int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
 
     static int quantidadeCandidatos = 0;
+    static int lerInteiro(String mensagem) {
+        while (true) {
+            System.out.print(mensagem);
+
+            if (scanner.hasNextInt()) {
+                int valor = scanner.nextInt();
+                scanner.nextLine();
+                return valor;
+            }
+
+            System.out.println("Entrada inválida. Digite um número.");
+            scanner.nextLine();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
@@ -33,7 +47,7 @@ public class SistemaVotacao {
             System.out.println("5 - Sair");
 
             System.out.print("Opção: ");
-            opcao = scanner.nextInt();
+            opcao = lerInteiro("Opção: ");
 
             switch (opcao) {
                 case 1:
@@ -55,6 +69,7 @@ public class SistemaVotacao {
                     System.out.println("Opção inválida.");
             }
         } while (opcao != 5);
+
 
 
     }
